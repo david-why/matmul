f0ad2cd5fa6a0f14504f92eb79dac3142af2ed3e
david-why <david_why@outlook.com>
2025-06-23 16:13:38 -0700

    make it 8 bits

--- hardware/accelerator.v
+++ hardware/accelerator.v
@@ -1,7 +1,8 @@
 module accelerator #(
     parameter ADDR_WRITE = 'h1100000,
     parameter ADDR_READ = 'h1300000,
-    parameter N = 3
+    parameter ADDR_END = 'h1500000,
+    parameter N = 8
 ) (
     input clk,
 
@@ -13,7 +14,11 @@ module accelerator #(
     output reg [31:0] mem_rdata
 );
 
-    reg [32*N-1:0] items = 0;
+    // storage
+
+    reg [32*N-1:0] items;
+
+    // calculation
 
     wire [64*N-1:0] mul_result;
     wire [63:0] result;
@@ -25,30 +30,28 @@ module accelerator #(
             assign mul_result[64*i+63:64*i] = mul_result[64*i-1:64*i-64] * items[32*i+31:32*i];
         end
     endgenerate
+
     assign result = mul_result[64*N-1:64*N-64];
+
+    // memory interface
 
     always @(posedge clk) begin
         if (mem_valid) begin
             if (mem_wstrb == 0) begin
                 // reading data
-                if (mem_addr == ADDR_READ) begin
-                    mem_rdata <= result[31:0];
+                if (mem_addr >= ADDR_READ && mem_addr < ADDR_END) begin
+                    mem_rdata <= result[(mem_addr&'hFFFFF)*8 +: 32];
                     mem_ready <= 1;
-                end else if (mem_addr == ADDR_READ + 4) begin
-                    mem_rdata <= result[63:32];
-                    mem_ready <= 1;
+                end else begin
+                    mem_ready <= 0;
                 end
             end else begin
                 // writing data
-                if (mem_addr == ADDR_WRITE) begin
-                    items[31:0] <= mem_wdata;
+                if (mem_addr >= ADDR_WRITE && mem_addr < ADDR_READ) begin
+                    items[(mem_addr&'hFFFFF)*8 +: 32] <= mem_wdata;
                     mem_ready <= 1;
-                end else if (mem_addr == ADDR_WRITE + 4) begin
-                    items[63:32] <= mem_wdata;
-                    mem_ready <= 1;
-                end else if (mem_addr == ADDR_WRITE + 8) begin
-                    items[95:64] <= mem_wdata;
-                    mem_ready <= 1;
+                end else begin
+                    mem_ready <= 0;
                 end
             end
         end else begin
--- simulated/main.c
+++ simulated/main.c
@@ -2,6 +2,8 @@
 
 #include <string.h>
 
+#define ACC_CHUNK_SIZE 8
+
 volatile uint32_t *const acc_in = (volatile uint32_t *)((void *)IO_ACC_WRITE);
 volatile const uint32_t *const acc_out = (const volatile uint32_t *)IO_ACC_READ;
 
@@ -17,16 +19,19 @@ uint64_t multiply_with_naive(const int *data, int n)
 
 uint64_t multiply_with_accelerator(const int *data, int n)
 {
-    if (n % 3)
+    if (n % ACC_CHUNK_SIZE)
     {
         return -1;
     }
     uint64_t val = 1;
-    for (int i = 0; i < n / 3; i++)
+    for (int i = 0; i < n / ACC_CHUNK_SIZE; i++)
     {
-        *acc_in = data[i*3 + 0];
-        *(acc_in+1) = data[i*3 + 1];
-        *(acc_in+2) = data[i*3 + 2];
+        // *acc_in = data[i*3 + 0];
+        // *(acc_in+1) = data[i*3 + 1];
+        // *(acc_in+2) = data[i*3 + 2];
+        for (int j = 0; j < ACC_CHUNK_SIZE; j++) {
+            acc_in[j] = data[i*ACC_CHUNK_SIZE + j];
+        }
         val *= *(volatile uint64_t *)acc_out;
     }
     return val;
@@ -34,20 +39,14 @@ uint64_t multiply_with_accelerator(const int *data, int n)
 
 int main()
 {
-    // printf("Writing to %x (%x)...\n", (int)acc_in, (int)IO_ACC_WRITE);
-    // acc_in[0] = 5;
-    // acc_in[1] = 5;
-    // acc_in[2] = 5;
-    // printf("Write complete!\n", acc_in);
-    // printf("Hello, World: %d!\n", *acc_out);
-    const static int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    const static int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
     uint64_t naive0 = getcycles();
-    int naive_result = multiply_with_naive(data, 9);
+    int naive_result = multiply_with_naive(data, 16);
     uint64_t naive1 = getcycles();
  
     uint64_t acc0 = getcycles();
-    int acc_result = multiply_with_accelerator(data, 9);
+    int acc_result = multiply_with_accelerator(data, 16);
     uint64_t acc1 = getcycles();
 
     printf("naive: %d, %d\n", naive_result, naive1 - naive0);
